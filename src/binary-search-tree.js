f8e649d5dc234592e109f09d04cae867b13a2dd0
Aleksey <alecsiko@gmail.com>
2022-10-17 23:24:57 +0300

    feat:first task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,145 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 class BinarySearchTree {
+  constructor(){
+    this.rootTwo = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootTwo;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootTwo = addWithin(this.rootTwo, data);
+    function addWithin(nodeAdd, data) {
+      if(!nodeAdd){
+        return new Node(data)
+      }
+    
+      if(nodeAdd.data === data){
+        return nodeAdd;
+      }
+      if(data < nodeAdd.data){
+        nodeAdd.left = addWithin(nodeAdd.left, data);
+      } else {
+        nodeAdd.right = addWithin(nodeAdd.right, data);
+      }
+      return nodeAdd;
+    }  
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.rootTwo, data);
+
+    function searchWithin(nodeHas, data) {
+      if(!nodeHas){
+        return false;
+      }
+
+      if(nodeHas.data === data){
+        return true;
+      }
+
+      return data < nodeHas.data ?
+        searchWithin(nodeHas.left, data):
+        searchWithin(nodeHas.right, data);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find( data ) {
+    return addNewFind(this.rootTwo, data);
+    function addNewFind(nodeFind, data) {
+      if (!nodeFind) {
+        return null;
+      }
+      if (nodeFind.data === data) {
+        return nodeFind;
+      }
+      if (data > nodeFind.data) {
+        return addNewFind(nodeFind.right, data);
+      }
+      if (data < nodeFind.data) {
+        return addNewFind(nodeFind.left, data);
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove( data ) {
+    this.rootTwo = addNewRemove(this.rootTwo, data);
+
+    function addNewRemove(nodeRemove, data) {
+      if (!nodeRemove) {
+        return null;
+      }
+
+      if (data > nodeRemove.data) {
+        nodeRemove.right = addNewRemove(nodeRemove.right, data);
+        return nodeRemove;
+      } else if (data < nodeRemove.data) {
+        nodeRemove.left = addNewRemove(nodeRemove.left, data);
+        return nodeRemove;
+      } else {
+        if (!nodeRemove.right && !nodeRemove.left) {
+          return null;
+        }
+        if (!nodeRemove.right) {
+          nodeRemove = nodeRemove.left;
+          return nodeRemove;
+        }
+        if (!nodeRemove.left) {
+          nodeRemove = nodeRemove.right;
+          return nodeRemove;
+        }
+      
+        let greatestAreaLeft = nodeRemove.left;
+
+        while (greatestAreaLeft.right) {
+          greatestAreaLeft = greatestAreaLeft.right;
+        }
+
+        nodeRemove.data = greatestAreaLeft.data;
+
+        nodeRemove.left = addNewRemove(nodeRemove.left, greatestAreaLeft.data);
+
+        return nodeRemove;
+        
+      }
+    }
   }
+  
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.rootTwo){
+      return
+    }
+     
+    let node = this.rootTwo;
+    while(node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.rootTwo){
+      return
+    }
+     
+    let node = this.rootTwo;
+    while(node.right) {
+      node = node.right;
+    }
+
+    return node.data;
   }
 }
 
